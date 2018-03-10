97ece74601aa7e446ced713e986f5016c4250c96
pipopotamasu <ec0156hx@gmail.com>
2018-03-10 18:06:54 +0900

    refactoring

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,75 @@ import {
   View
 } from 'react-native';
 import Todos from './todo/Todos';
+import Store from 'react-native-store';
 
+const DB = {
+  'todo': Store.model('todo')
+}
+
+const INITIAL_TODOS = [{ id: 1, text: 'test1', done: false, created_at: null },
+                       { id: 2, text: 'test2', done: false, created_at: null },
+                       { id: 3, text: 'test3', done: false, created_at: null },
+                       { id: 4, text: 'test4', done: false, created_at: null },
+                       { id: 5, text: 'test5', done: false, created_at: null }]
 
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+    };
+  }
+
+  async componentWillMount() {
+    const today = this.props.today
+
+    const res = await DB.todo.find({
+        where: {
+          created_at: today
+        },
+        order: {
+            id: 'ASC',
+        }
+    })
+
+    // exist today's todos?
+    if (res === null) {
+      // register today's todos if not exists
+      await this.saveTodos(this.state.todos)
+      this.setState({todos: INITIAL_TODOS})
+    } else {
+      // fetch today's todos if exists
+      this.setState({
+        todos: res
+      });
+    }
+  }
+
+  async saveTodos(todos) {
+    const today = this.props.today
+    for(let i in INITIAL_TODOS) {
+      await DB.todo.add(Object.assign(INITIAL_TODOS[i], { created_at: today }))
+    }
+  }
+
+  async updateTodo(todo) {
+    await DB.todo.updateById(todo, todo._id)
+  }
+
+  _toggle = (index) => () => {
+    const todos = [].concat(this.state.todos);
+    todos[index].done = !todos[index].done;
+
+    this.updateTodo(todos[index])
+
+    this.setState({
+      todos,
+    });
+  }
+
   today() {
     const today = new Date()
     const year = today.getFullYear()
@@ -19,6 +84,10 @@ export default class App extends Component {
   }
 
   render() {
+    const {
+      todos
+    } = this.state
+
     return (
       <View style={styles.container}>
         <View style={styles.dateContainer}>
@@ -29,7 +98,8 @@ export default class App extends Component {
         </View>
         <View style={styles.todosContainer}>
           <Todos
-            today={this.today()}
+            todos={todos}
+            onToggle={this._toggle}
           ></Todos>
         </View>
       </View>
