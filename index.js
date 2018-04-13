85d522588b2a5c6a4b3c0e4d4bd1d4d1d913949e
pipopotamasu <ec0156hx@gmail.com>
2018-04-14 00:02:39 +0900

    regist navigation change hook

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/Navigation';
 
-AppRegistry.registerComponent('calendar', () => App);
+class RootComponent extends Component {
+  render() {
+    return <App onNavigationStateChange={this.hoge}/>
+  }
+
+  hoge(prevState, currentState, action) {
+    console.log(prevState)
+    console.log('hoge')
+  }
+}
+
+AppRegistry.registerComponent('calendar', () => RootComponent);
